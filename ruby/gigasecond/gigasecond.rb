b3eb78b971bfe0041e05b8aaa478a8098c17a444
Syeda Ismat Farjana <diptyfmc17@gmail.com>
2020-04-16 23:28:59 +1000

    Gigasecond solution

---
+++ ruby/gigasecond/gigasecond.rb
@@ -0,0 +1,14 @@
+=begin
+Write your code for the 'Gigasecond' exercise in this file. Make the tests in
+`gigasecond_test.rb` pass.
+
+To get started with TDD, see the `README.md` file in your
+`ruby/gigasecond` directory.
+=end
+require 'date'
+require 'time'
+module Gigasecond
+  def self.from(date)
+    date +(10**9)
+  end
+end
