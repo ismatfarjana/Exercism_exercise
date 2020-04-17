f7077a38958c80fd5584aa0d1c73322b7424c0da
Syeda Ismat Farjana <diptyfmc17@gmail.com>
2020-04-18 01:00:00 +1000

    exercism solution of acronym.rb

---
+++ ruby/acronym/acronym.rb
@@ -0,0 +1,13 @@
+=begin
+Write your code for the 'Acronym' exercise in this file. Make the tests in
+`acronym_test.rb` pass.
+
+To get started with TDD, see the `README.md` file in your
+`ruby/acronym` directory.
+=end
+
+module Acronym
+  def self.abbreviate(string)
+    string.sub("-", " ").split(" ").map {|word| word.split("").first}.join("").upcase
+  end
+end
